d7322118b431a9b35362624bb303101d9be22069
Alan <alanondiscord@gmail.com>
2026-05-27 22:32:39 +0200

    chore: setup sphere module

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,8 +3,13 @@ game("gta5")
 description("Battleroyale script with an advanced, clean and optimized system. Designed for a fun experience for your players.")
 authors("Alan")
 version("1.0.0")
+
+shared_scripts({
+    "@ox_lib/init.lua",
+})
+
 client_scripts({
-	"server/**.lua"
+	"client/**.lua"
 })
 
 server_script({
